df21128a9a00faab115efdce965c3c9617531f2e
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-04 23:26:45 -0700

    Add files via upload

---
+++ Monster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventureBuilder02
+{
+    public class Monster
+    {
+        public string Name;
+
+        public Monster(string n)
+        {
+            this.Name = n;
+        }
+    }
+}
